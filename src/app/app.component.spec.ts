e38ad790d067a2de90c93796ac04545b74a45022
Emmanuel <koomsonthree@gmail.com>
2024-12-21 06:16:13 -0800

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,7 +4,13 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+<<<<<<< HEAD
       imports: [AppComponent],
+=======
+      declarations: [
+        AppComponent
+      ],
+>>>>>>> ff89c4e (Initial commit)
     }).compileComponents();
   });
 
@@ -14,16 +20,27 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
+<<<<<<< HEAD
   it(`should have the 'my-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('my-app');
+=======
+  it(`should have as title 'cms'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('cms');
+>>>>>>> ff89c4e (Initial commit)
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
+<<<<<<< HEAD
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-app');
+=======
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cms app is running!');
+>>>>>>> ff89c4e (Initial commit)
   });
 });
